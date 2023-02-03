8cd77b679f01432149ff128cb4c3ae19963f22f3
maruthiarya145@gmail.com <maruthiarya145@gmail.com>
2023-02-03 12:03:29 +0530

    first commit

---
+++ src/maruthi/ma.js
@@ -0,0 +1,18 @@
+export const App1=()=>{
+    return(
+        <div>
+            <center style={{color:"blue",fontSize:"x-large"}}><h1>Registration</h1></center>
+           <center>
+           <form>
+                
+            </form>
+           </center>
+            <div className="card">
+                <div className="card-body">
+<h1 className="card-title">hero</h1>
+<h2 className="card-text"> maruthi jaja jajajaja</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
